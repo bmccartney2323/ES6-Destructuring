67618d5cea9b12ea5ffca33140516d25eea554d5
bmccartney2323 <65781099+bmccartney2323@users.noreply.github.com>
2020-11-09 16:22:05 -0500

    ES6 Destructuring  Complete

--- src/index.js
+++ src/index.js
@@ -1,23 +1,49 @@
-// CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+// import animals, {useAnimals} from "./data";
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+//Destructure arrays
+//console.log(animals);
+// const [cat, dog] = animals;
+//console.log(cat);
+//const { name ="Fluffy", sound = "Purr" } = cat;
+
+// const [animal, makeSound] = useAnimals(cat);
+// console.log(animal);
+// makeSound();
+//Destructure objects
+//const {name, sound, feedingRequirements: {food, water}} =cat;
+//console.log(feedingRequirements);
+//console.log(food);
+
+// CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
+
+const [honda, tesla] = cars;
+
+const {speedStats: {topSpeed: hondaTopSpeed}} = honda;
+const {speedStats: {topSpeed: teslaTopSpeed}} = tesla;
+
+const {coloursByPopularity: [teslaTopColour]} = tesla;
+
+const {coloursByPopularity: [hondaTopColour]} = honda;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
